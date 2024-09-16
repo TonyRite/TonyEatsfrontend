02ca053aee9dd9c0ed1fc4c637d73b53ad738628
TonyRite <lioswinrite@gmail.com>
2024-09-16 09:38:34 +0300

    added checkout workflow

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,6 +6,7 @@ import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/page";
+import DetailPage from "./pages/DetailPage";
 
 const AppRoutes = () => {
     return (
@@ -24,7 +25,16 @@ const AppRoutes = () => {
             element={
             <Layout showHero={false}>
                 <SearchPage/>
-            </Layout>}/>
+            </Layout>}
+            />
+
+            <Route path="/detail/:restaurantId" 
+                 element={
+                    <Layout showHero={false}>
+                        <DetailPage/>
+                    </Layout>}
+               />
+
             <Route element={<ProtectedRoute/>}>
             <Route 
             path='/user-profile' 
